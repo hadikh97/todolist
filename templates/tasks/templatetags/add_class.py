60fdfcab412800a3747e43e914d92455afcb7dc7
Hadi <hadi.khashei.1376@gmail.com>
2024-06-15 03:12:48 +0330

    add class

--- templates/tasks/templatetags/add_class.py
+++ templates/tasks/templatetags/add_class.py
@@ -5,3 +5,10 @@ register = template.Library()
 @register.filter(name='add_class')
 def add_class(value, arg):
     return value.as_widget(attrs={'class': arg})
+from django import template
+
+register = template.Library()
+
+@register.filter(name='add_class')
+def add_class(value, arg):
+    return value.as_widget(attrs={'class': arg})
